18a3520178c9a3048e6b7214d4685c5de74a9a9a
m4tyn0 <coufal.matej@gmail.com>
2022-02-13 17:47:23 +0100

    draft

---
+++ TaskManager/Controllers/TaskManagerController.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Models;
+
+namespace TaskManager.Controllers
+{
+    public class TaskManagerController : Controller
+    {
+        private readonly TaskManagerContext context;
+
+        public TaskManagerController(TaskManagerContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+             return View(await this.context.TaskUnit.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var taskUnit = await this.context.TaskUnit
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (taskUnit == null)
+            {
+                return NotFound();
+            }
+
+            return View(taskUnit);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Content,tag")] TaskUnit taskUnit)
+        {
+            if (ModelState.IsValid)
+            {
+                this.context.Add(taskUnit);
+                await this.context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(taskUnit);
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var taskUnit = await this.context.TaskUnit.FindAsync(id);
+            if (taskUnit == null)
+            {
+                return NotFound();
+            }
+            return View(taskUnit);
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Name,Content,tag")] TaskUnit taskUnit)
+        {
+            if (id != taskUnit.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    this.context.Update(taskUnit);
+                    await this.context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TaskUnitExists(taskUnit.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(taskUnit);
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var taskUnit = await this.context.TaskUnit
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (taskUnit == null)
+            {
+                return NotFound();
+            }
+
+            return View(taskUnit);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var taskUnit = await this.context.TaskUnit.FindAsync(id);
+            this.context.TaskUnit.Remove(taskUnit);
+            await this.context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TaskUnitExists(string id)
+        {
+            return this.context.TaskUnit.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ TaskManager/Models/TaskUnit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace TaskManager.Models
+{
+    public class TaskUnit
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public string tag { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime TimeCreated { get; }
+        [DataType(DataType.DateTime)]
+        protected TimeSpan TimeElapsed { get; set; }
+
+
+
+        public TaskUnit()
+        {
+            TimeCreated = DateTime.Now;
+            TimeElapsed = ReturnElapsed();
+            
+
+
+        }
+
+
+        public TimeSpan ReturnElapsed()
+        {
+            return this.TimeElapsed = this.TimeCreated - DateTime.Now;
+
+        }
+    }
+}
+//dotnet-aspnet-codegenerator controller -name TaskManagerController -m TaskUnit -dc TaskManagerContext --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries -sqlite -f

d48a86a0cbd2fff6caf6fc1be293ff62c66004a6
Matěj <99069615+m4tyn0@users.noreply.github.com>
2022-02-14 16:46:59 +0100

    cosmetix
    small cosmetic changes

--- TaskManager/Models/TaskUnit.cs
+++ TaskManager/Models/TaskUnit.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
-
+using System.Diagnostics;
 namespace TaskManager.Models
 {
     public class TaskUnit
@@ -14,28 +14,17 @@ namespace TaskManager.Models
         public string Name { get; set; }
         public string Content { get; set; }
         public string tag { get; set; }
-    
         public DateTime TimeCreated { get; set; }
        
-        protected TimeSpan TimeElapsed { get; set; }
-
 
 
         public TaskUnit()
         {
-            TimeCreated = DateTime.Now;
-            TimeElapsed = ReturnElapsed();
-            
 
 
         }
 
 
-        public TimeSpan ReturnElapsed()
-        {
-            return this.TimeElapsed = this.TimeCreated - DateTime.Now;
-
-        }
+       
     }
 }
-//dotnet-aspnet-codegenerator controller -name TaskManagerController -m TaskUnit -dc TaskManagerContext --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries -sqlite -f
